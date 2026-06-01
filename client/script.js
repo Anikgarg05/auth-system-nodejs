9506bddcedffd3cc7c286adcf14657cf530ea79b
Anik_garg <garganik05@gmail.com>
2026-06-01 16:19:45 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,13 +1,12 @@
+const API_URL = "https://auth-system-nodejs-production-2a4a.up.railway.app";
+
 // SIGNUP FUNCTION
 window.signup = function () {
     console.log("Signup clicked");
-
     const email = document.getElementById("signupEmail").value;
     const password = document.getElementById("signupPassword").value;
-
     console.log("Data:", email, password);
-
-    fetch("http://localhost:5000/signup", {
+    fetch(`${API_URL}/signup`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -20,24 +19,20 @@ window.signup = function () {
     })
     .then(data => {
         console.log("Response:", data);
-        alert(data);   // shows Signup successful
+        alert(data);
     })  
     .catch(err => {
         console.log("ERROR:", err);
     });
 };
 
-
 // LOGIN FUNCTION
 window.login = function () {
     console.log("Login clicked");
-
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPassword").value;
-
     console.log("Data:", email, password);
-
-    fetch("http://localhost:5000/login", {
+    fetch(`${API_URL}/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -50,7 +45,7 @@ window.login = function () {
     })
     .then(data => {
         console.log("Response:", data);
-        alert(data);   // shows Login result
+        alert(data);
     })
     .catch(err => {
         console.log("ERROR:", err);
