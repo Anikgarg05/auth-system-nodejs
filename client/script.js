bd65229bff03dd86afd91a15673576e0714e3017
Anik_garg <garganik05@gmail.com>
2026-06-01 16:52:15 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,53 +1,60 @@
 const API_URL = "https://auth-system-nodejs-production-2a4a.up.railway.app";
 
 // SIGNUP FUNCTION
-window.signup = function () {
+window.signup = async function () {
     console.log("Signup clicked");
+
     const email = document.getElementById("signupEmail").value;
     const password = document.getElementById("signupPassword").value;
-    console.log("Data:", email, password);
-    fetch(`${API_URL}/signup`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-    })
-    .then(res => {
-        console.log("Fetch executed");
-        return res.text();
-    })
-    .then(data => {
+
+    try {
+        const res = await fetch(`${API_URL}/signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await res.text();
+
         console.log("Response:", data);
         alert(data);
-    })  
-    .catch(err => {
+
+        // clear input after success
+        if (data === "Signup successful") {
+            document.getElementById("signupEmail").value = "";
+            document.getElementById("signupPassword").value = "";
+        }
+
+    } catch (err) {
         console.log("ERROR:", err);
-    });
+    }
 };
 
+
 // LOGIN FUNCTION
-window.login = function () {
+window.login = async function () {
     console.log("Login clicked");
+
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPassword").value;
-    console.log("Data:", email, password);
-    fetch(`${API_URL}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-    })
-    .then(res => {
-        console.log("Fetch executed");
-        return res.text();
-    })
-    .then(data => {
+
+    try {
+        const res = await fetch(`${API_URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await res.text();
+
         console.log("Response:", data);
         alert(data);
-    })
-    .catch(err => {
+
+    } catch (err) {
         console.log("ERROR:", err);
-    });
+    }
 };
